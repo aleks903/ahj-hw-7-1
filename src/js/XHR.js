c641bad94ca585f14b9abb10cf54ffb3d8814b2b
Aleksandr K-v <aleks903@rambler.ru>
2020-02-17 11:52:33 +0300

    ahj-hw-7-1

---
+++ src/js/XHR.js
@@ -0,0 +1,120 @@
+/* eslint-disable class-methods-use-this */
+export default class XHR {
+  addTicket(name, description) {
+    return new Promise((resolve) => {
+      const params = new URLSearchParams();
+      params.append('name', name);
+      params.append('description', description);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:7070/tickets');
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          return resolve(xhr.responseText);
+        }
+        // TODO: handle other status
+        console.log(xhr.responseText);
+        return xhr.responseText;
+      });
+      xhr.send(params);
+    });
+  }
+
+  getTickets() {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'http://localhost:7070/tickets');
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          const tickets = JSON.parse(xhr.responseText);
+          return resolve(tickets);
+        }
+        console.log(xhr.responseText);
+        return xhr.responseText;
+      });
+      xhr.send();
+    });
+  }
+
+  changeTickets(id, name, description) {
+    return new Promise((resolve) => {
+      const params = new URLSearchParams();
+      params.append('id', id);
+      params.append('name', name);
+      params.append('description', description);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('PUT', 'http://localhost:7070/tickets');
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          return resolve(xhr.responseText);
+        }
+        // TODO: handle other status
+        console.log(xhr.responseText);
+        return xhr.responseText;
+      });
+      xhr.send(params);
+    });
+  }
+
+  getDescription(id) {
+    return new Promise((resolve) => {
+      const params = new URLSearchParams();
+      params.append('id', id);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `http://localhost:7070/tickets?${params}`);
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          const tickets = xhr.responseText;
+          return resolve(tickets);
+        }
+        // TODO: handle other status
+        console.log(xhr.responseText);
+        return xhr.responseText;
+      });
+      xhr.send();
+    });
+  }
+
+  delTicket(id) {
+    return new Promise((resolve) => {
+      const params = new URLSearchParams();
+      params.append('id', id);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('DELETE', `http://localhost:7070/tickets?${params}`);
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          const tickets = xhr.responseText;
+          return resolve(tickets);
+        }
+        // TODO: handle other status
+        console.log(xhr.responseText);
+        return xhr.responseText;
+      });
+      xhr.send();
+    });
+  }
+
+  changeStatus(id, status) {
+    return new Promise((resolve) => {
+      const params = new URLSearchParams();
+      params.append('id', id);
+      params.append('status', status);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('PATCH', `http://localhost:7070/tickets?${params}`);
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          const tickets = xhr.responseText;
+          return resolve(tickets);
+        }
+        // TODO: handle other status
+        console.log(xhr.responseText);
+        return xhr.responseText;
+      });
+      xhr.send();
+    });
+  }
+}
